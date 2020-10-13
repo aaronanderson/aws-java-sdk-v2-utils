9484ac08f33d7859587e2191fc381fe58f8b4b5f
Aaron Anderson <aaronanderson@acm.org>
2020-10-13 15:21:46 -0500

    Updated with request body example

--- src/main/java/AWSSignerHttpClient.java
+++ src/main/java/AWSSignerHttpClient.java
@@ -1,3 +1,4 @@
+import java.time.Duration;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -25,7 +26,9 @@ import software.amazon.awssdk.core.internal.http.AmazonSyncHttpClient;
 import software.amazon.awssdk.core.internal.http.CombinedResponseHandler;
 import software.amazon.awssdk.core.internal.http.loader.DefaultSdkHttpClientBuilder;
 import software.amazon.awssdk.core.retry.RetryPolicy;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.SdkHttpConfigurationOption;
 import software.amazon.awssdk.http.SdkHttpFullRequest;
 import software.amazon.awssdk.http.SdkHttpFullResponse;
 import software.amazon.awssdk.metrics.NoOpMetricCollector;
@@ -36,146 +39,157 @@ import software.amazon.awssdk.utils.IoUtils;
 
 public class AWSSignerHttpClient implements AutoCloseable {
 
-	private String serviceName;
-	private Region region;
-	private AwsCredentialsProvider awsCredentialsProvider;
-	private SdkHttpClient sdkClient;
-	// required by client to avoid NPE
-	private SdkRequest sdkRequest = new ServiceSDKRequest();
-	private ExecutionInterceptorChain execInterceptorChain = new ExecutionInterceptorChain(Collections.emptyList());
-	private AmazonSyncHttpClient awsClient;
-	private Aws4Signer signer;
+    private String serviceName;
+    private Region region;
+    private AwsCredentialsProvider awsCredentialsProvider;
+    private Duration readTimeOut;
+    private SdkHttpClient sdkClient;
+    // required by client to avoid NPE
+    private SdkRequest sdkRequest = new ServiceSDKRequest();
+    private ExecutionInterceptorChain execInterceptorChain = new ExecutionInterceptorChain(Collections.emptyList());
+    private AmazonSyncHttpClient awsClient;
+    private Aws4Signer signer;
 
-	private AWSSignerHttpClient() {
+    private AWSSignerHttpClient() {
 
-	}
+    }
 
-	public static Builder builder() {
-		return new Builder();
-	}
+    public static Builder builder() {
+        return new Builder();
+    }
 
-	public <T> T execute(SdkHttpFullRequest httpRequest, HttpResponseHandler<T> responseHandler) {
-		return execute(httpRequest, responseHandler, new ErrorHandler());
-	}
+    public <T> T execute(SdkHttpFullRequest httpRequest, RequestBody requestBody, HttpResponseHandler<T> responseHandler) {
+        return execute(httpRequest, requestBody, responseHandler, new ErrorHandler());
+    }
 
-	public <T extends JsonStructure> T execute(SdkHttpFullRequest httpRequest) {
-		return execute(httpRequest, new JsonHandler<T>(), new ErrorHandler());
-	}
+    public <T extends JsonStructure> T execute(SdkHttpFullRequest httpRequest, RequestBody requestBody) {
+        return execute(httpRequest, requestBody, new JsonHandler<T>(), new ErrorHandler());
+    }
 
-	public <T> T execute(SdkHttpFullRequest httpRequest, HttpResponseHandler<T> responseHandler, HttpResponseHandler<? extends SdkException> errorHandler) {
-		InterceptorContext incerceptorContext = InterceptorContext.builder().request(sdkRequest).httpRequest(httpRequest).build();
-		ExecutionContext.Builder execContextBuilder = ExecutionContext.builder();
-		execContextBuilder.signer(signer);
-		execContextBuilder.interceptorChain(execInterceptorChain);
-		execContextBuilder.metricCollector(NoOpMetricCollector.create());
-		ExecutionAttributes executionAttributes = new ExecutionAttributes();
-		executionAttributes.putAttribute(AwsSignerExecutionAttribute.AWS_CREDENTIALS, awsCredentialsProvider.resolveCredentials());
-		executionAttributes.putAttribute(AwsSignerExecutionAttribute.SERVICE_SIGNING_NAME, serviceName);
-		executionAttributes.putAttribute(AwsSignerExecutionAttribute.SIGNING_REGION, region);
-		execContextBuilder.executionAttributes(executionAttributes);
-		execContextBuilder.interceptorContext(incerceptorContext).build();
-		ExecutionContext execContext = execContextBuilder.build();
-		return awsClient.requestExecutionBuilder().executionContext(execContext).originalRequest(sdkRequest).request(httpRequest).execute(new CombinedResponseHandler<T>(responseHandler, errorHandler));
-	}
+    public <T extends JsonStructure> T execute(SdkHttpFullRequest httpRequest) {
+        return execute(httpRequest, null, new JsonHandler<T>(), new ErrorHandler());
+    }
 
-	public static class Builder {
-		AWSSignerHttpClient client = new AWSSignerHttpClient();
+    public <T> T execute(SdkHttpFullRequest httpRequest, RequestBody requestBody, HttpResponseHandler<T> responseHandler, HttpResponseHandler<? extends SdkException> errorHandler) {
+        InterceptorContext incerceptorContext = InterceptorContext.builder().requestBody(requestBody).request(sdkRequest).httpRequest(httpRequest).build();
+        ExecutionContext.Builder execContextBuilder = ExecutionContext.builder();
+        execContextBuilder.signer(signer);
+        execContextBuilder.interceptorChain(execInterceptorChain);
+        execContextBuilder.metricCollector(NoOpMetricCollector.create());
+        ExecutionAttributes executionAttributes = new ExecutionAttributes();
+        executionAttributes.putAttribute(AwsSignerExecutionAttribute.AWS_CREDENTIALS, awsCredentialsProvider.resolveCredentials());
+        executionAttributes.putAttribute(AwsSignerExecutionAttribute.SERVICE_SIGNING_NAME, serviceName);
+        executionAttributes.putAttribute(AwsSignerExecutionAttribute.SIGNING_REGION, region);
+        execContextBuilder.executionAttributes(executionAttributes);
+        execContextBuilder.interceptorContext(incerceptorContext).build();
+        ExecutionContext execContext = execContextBuilder.build();
+        return awsClient.requestExecutionBuilder().executionContext(execContext).originalRequest(sdkRequest).request(httpRequest).execute(new CombinedResponseHandler<T>(responseHandler, errorHandler));
+    }
 
-		public AWSSignerHttpClient build() {
-			if (client.awsCredentialsProvider == null) {
-				DefaultCredentialsProvider provider = DefaultCredentialsProvider.create();
-				client.awsCredentialsProvider = provider;
-			}
-			if (client.sdkClient == null) {
-				client.sdkClient = new DefaultSdkHttpClientBuilder().buildWithDefaults(AttributeMap.empty());
-			}
+    public static class Builder {
+        AWSSignerHttpClient client = new AWSSignerHttpClient();
 
-			if (client.region == null) {
-				client.region = new DefaultAwsRegionProviderChain().getRegion();
-			}
-			client.signer = Aws4Signer.create();
-			// signer.setRegionName(client.region.value());
-			// signer.setServiceName(client.serviceName);
-			// client.signingProvider = StaticSignerProvider.create(signer);
-			SdkClientConfiguration clientConfiguration = SdkClientConfiguration.builder().option(SdkClientOption.ADDITIONAL_HTTP_HEADERS, new LinkedHashMap<>()).option(SdkClientOption.CRC32_FROM_COMPRESSED_DATA_ENABLED, true).option(SdkClientOption.SYNC_HTTP_CLIENT, client.sdkClient).option(SdkClientOption.RETRY_POLICY, RetryPolicy.none()).build();
-			client.awsClient = new AmazonSyncHttpClient(clientConfiguration);
-			return client;
-		}
+        public AWSSignerHttpClient build() {
+            if (client.awsCredentialsProvider == null) {
+                DefaultCredentialsProvider provider = DefaultCredentialsProvider.create();
+                client.awsCredentialsProvider = provider;
+            }
+            if (client.sdkClient == null) {
+                AttributeMap options = client.readTimeOut != null ? AttributeMap.builder().put(SdkHttpConfigurationOption.READ_TIMEOUT, client.readTimeOut).build() : AttributeMap.empty();
+                client.sdkClient = new DefaultSdkHttpClientBuilder().buildWithDefaults(options);
+            }
 
-		public Builder sdkClient(SdkHttpClient sdkClient) {
-			client.sdkClient = sdkClient;
-			return this;
-		}
+            if (client.region == null) {
+                client.region = new DefaultAwsRegionProviderChain().getRegion();
+            }
+            client.signer = Aws4Signer.create();
+            // signer.setRegionName(client.region.value());
+            // signer.setServiceName(client.serviceName);
+            // client.signingProvider = StaticSignerProvider.create(signer);
+            SdkClientConfiguration clientConfiguration = SdkClientConfiguration.builder().option(SdkClientOption.ADDITIONAL_HTTP_HEADERS, new LinkedHashMap<>()).option(SdkClientOption.CRC32_FROM_COMPRESSED_DATA_ENABLED, true).option(SdkClientOption.SYNC_HTTP_CLIENT, client.sdkClient).option(SdkClientOption.RETRY_POLICY, RetryPolicy.none()).build();
+            client.awsClient = new AmazonSyncHttpClient(clientConfiguration);
+            return client;
+        }
 
-		public Builder serviceName(String serviceName) {
-			client.serviceName = serviceName;
-			return this;
-		}
+        public Builder sdkClient(SdkHttpClient sdkClient) {
+            client.sdkClient = sdkClient;
+            return this;
+        }
 
-		public Builder region(Region region) {
-			client.region = region;
-			return this;
-		}
+        public Builder serviceName(String serviceName) {
+            client.serviceName = serviceName;
+            return this;
+        }
 
-		public Builder awsCredentials(AwsCredentialsProvider awsCredentialsProvider) {
-			client.awsCredentialsProvider = awsCredentialsProvider;
-			return this;
-		}
-	}
+        public Builder region(Region region) {
+            client.region = region;
+            return this;
+        }
 
-	public static class ServiceSDKRequest extends SdkRequest {
+        public Builder awsCredentials(AwsCredentialsProvider awsCredentialsProvider) {
+            client.awsCredentialsProvider = awsCredentialsProvider;
+            return this;
+        }
 
-		@Override
-		public Optional<? extends RequestOverrideConfiguration> overrideConfiguration() {
-			return Optional.empty();
-		}
+        public Builder readTimeout(Duration readTimeOut) {
+            client.readTimeOut = readTimeOut;
+            return this;
+        }
+    }
 
-		@Override
-		public Builder toBuilder() {
-			return new Builder() {
+    public static class ServiceSDKRequest extends SdkRequest {
 
-				@Override
-				public RequestOverrideConfiguration overrideConfiguration() {
-					return null;
-				}
+        @Override
+        public Optional<? extends RequestOverrideConfiguration> overrideConfiguration() {
+            return Optional.empty();
+        }
 
-				@Override
-				public SdkRequest build() {
-					return new ServiceSDKRequest();
-				}
+        @Override
+        public Builder toBuilder() {
+            return new Builder() {
 
-			};
-		}
+                @Override
+                public RequestOverrideConfiguration overrideConfiguration() {
+                    return null;
+                }
 
-		@Override
-		public List<SdkField<?>> sdkFields() {
-			return Collections.unmodifiableList(Collections.emptyList());
-		}
+                @Override
+                public SdkRequest build() {
+                    return new ServiceSDKRequest();
+                }
 
-	}
+            };
+        }
 
-	public static class ErrorHandler implements HttpResponseHandler<SdkException> {
+        @Override
+        public List<SdkField<?>> sdkFields() {
+            return Collections.unmodifiableList(Collections.emptyList());
+        }
 
-		@Override
-		public SdkException handle(SdkHttpFullResponse response, ExecutionAttributes executionAttributes) throws Exception {
-			String responseMsg = response.content().isPresent() ? IoUtils.toUtf8String(response.content().get()) : "";
-			return SdkException.builder().message(String.format("%d: %s", response.statusCode(), responseMsg)).build();
-		}
+    }
 
-	}
+    public static class ErrorHandler implements HttpResponseHandler<SdkException> {
 
-	public static class JsonHandler<T extends JsonStructure> implements HttpResponseHandler<T> {
+        @Override
+        public SdkException handle(SdkHttpFullResponse response, ExecutionAttributes executionAttributes) throws Exception {
+            String responseMsg = response.content().isPresent() ? IoUtils.toUtf8String(response.content().get()) : "";
+            return SdkException.builder().message(String.format("%d: %s", response.statusCode(), responseMsg)).build();
+        }
 
-		@Override
-		public T handle(SdkHttpFullResponse response, ExecutionAttributes executionAttributes) throws Exception {
-			return response.content().isPresent() ? (T) Json.createReader(response.content().get()).read() : null;
-		}
+    }
 
-	}
+    public static class JsonHandler<T extends JsonStructure> implements HttpResponseHandler<T> {
 
-	@Override
-	public void close() {
-		awsClient.close();
+        @Override
+        public T handle(SdkHttpFullResponse response, ExecutionAttributes executionAttributes) throws Exception {
+            return response.content().isPresent() ? (T) Json.createReader(response.content().get()).read() : null;
+        }
 
-	}
+    }
+
+    @Override
+    public void close() {
+        awsClient.close();
+
+    }
 }
--- src/test/java/ElasticSearchTest.java
+++ src/test/java/ElasticSearchTest.java
@@ -1,5 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.time.Duration;
+
 import javax.json.JsonObject;
 
 import org.junit.jupiter.api.Test;
@@ -7,22 +9,38 @@ import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.http.SdkHttpFullRequest;
 import software.amazon.awssdk.http.SdkHttpMethod;
 
 public class ElasticSearchTest {
 
-	public static final String ELASTIC_SEARCH_HOST = "XXXXXXXXXXXXXX.us-west-1.es.amazonaws.com";
+    public static final String ELASTIC_SEARCH_HOST = "XXXXXXXXXXXXXX.us-west-1.es.amazonaws.com";    
 
+    //@Test
+    public void searchTest() throws Exception {
+        AWSSignerHttpClient client = AWSSignerHttpClient.builder().serviceName("es").awsCredentials(AwsCredentialsProviderChain.of(InstanceProfileCredentialsProvider.builder().build(), DefaultCredentialsProvider.create())).build();
+        SdkHttpFullRequest httpRequest = SdkHttpFullRequest.builder().method(SdkHttpMethod.GET).protocol("https").host(ELASTIC_SEARCH_HOST).appendRawQueryParameter("pretty", "true").build();
+        JsonObject obj = client.<JsonObject> execute(httpRequest);
+        assertNotNull(obj);
+        System.out.format("Result: %s\n", obj);
 
-	//@Test
-	public void searchTest() throws Exception {
-		AWSSignerHttpClient client = AWSSignerHttpClient.builder().serviceName("es").awsCredentials(AwsCredentialsProviderChain.of(InstanceProfileCredentialsProvider.builder().build(), DefaultCredentialsProvider.create())).build();
-		SdkHttpFullRequest httpRequest = SdkHttpFullRequest.builder().method(SdkHttpMethod.GET).protocol("https").host(ELASTIC_SEARCH_HOST).build();
-		JsonObject obj = client.<JsonObject>execute(httpRequest);
-		assertNotNull(obj);
-		System.out.format("Result: %s\n", obj);
+    }
 
-	}
+    //@Test
+    public void createIndexTest() throws Exception {
+        AWSSignerHttpClient client = AWSSignerHttpClient.builder().serviceName("es").readTimeout(Duration.ofMinutes(1)).awsCredentials(AwsCredentialsProviderChain.of(InstanceProfileCredentialsProvider.builder().build(), DefaultCredentialsProvider.create())).build();
+
+        RequestBody requestBody = RequestBody.fromString("{\"settings\": {  \"index\": { \"number_of_shards\": 5, \"number_of_replicas\": 1 } } }");
+        SdkHttpFullRequest httpRequest = SdkHttpFullRequest.builder().method(SdkHttpMethod.PUT).protocol("https").host(ELASTIC_SEARCH_HOST).encodedPath("/test-index").appendRawQueryParameter("pretty", "true").build();
+        JsonObject obj = client.<JsonObject> execute(httpRequest, requestBody);
+        assertNotNull(obj);
+        System.out.format("Result: %s\n", obj);
+
+        httpRequest = SdkHttpFullRequest.builder().method(SdkHttpMethod.DELETE).protocol("https").host(ELASTIC_SEARCH_HOST).encodedPath("/test-index").appendRawQueryParameter("pretty", "true").build();
+        obj = client.<JsonObject> execute(httpRequest);
+        assertNotNull(obj);
+        System.out.format("Result: %s\n", obj);
+    }
 
 }
